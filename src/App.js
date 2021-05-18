69f8daf8d98a9127f830e6f1294b5a42d3ec618d
loveneesh1716 <loveneesh1716.cse18@chitkara.edu.in>
2021-05-19 02:39:29 +0530

    adding increase and decrease functions in cart

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import CartItem from './CartItem'
+// functional based component
 function App() {
     return (
          <div className = "App">
-        <h1><center> CART </center></h1> </div >
+        <h1><center> CART </center></h1>
+        <CartItem/>
+         </div >
     );
 }
 
